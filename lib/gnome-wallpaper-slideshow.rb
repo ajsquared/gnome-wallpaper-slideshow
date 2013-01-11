845cee190d9997f9d0b5b3a6dc2f7bbe1d7648ca
Andy Johnson <theonceandfutureajsquared@gmail.com>
2013-01-11 18:25:17 -0500

    Changed to use filename

--- lib/gnome-wallpaper-slideshow.rb
+++ lib/gnome-wallpaper-slideshow.rb
@@ -5,8 +5,6 @@ require 'pathname'
 #
 # @author Andrew Johnson
 class GnomeWallpaperSlideshow
-  attr_reader :path, :xml_doc
-  
   # Initializes a new wallpaper slideshow
   # @param [String] filename The path to a wallpaper slideshow XML file.
   #  Either pass the path to a non-existent file or omit the argument to
@@ -15,7 +13,7 @@ class GnomeWallpaperSlideshow
     @path = Pathname.new filename
 
     if @path.exist?
-      File.open(filename, "r") do |slideshow_file|
+      File.open(@path, "r") do |slideshow_file|
         # We want strict parsing to ensure that we're given an actual XML file
         @xml_doc = Nokogiri::XML(slideshow_file) do |config|
           config.strict
