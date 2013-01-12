907f1fbf71a1370475514d2ef0846dbf4ce91baa
Andy Johnson <theonceandfutureajsquared@gmail.com>
2013-01-12 11:36:20 -0500

    Changed to instance_eval DSL

--- lib/gnome-wallpaper-slideshow.rb
+++ lib/gnome-wallpaper-slideshow.rb
@@ -5,19 +5,36 @@ require 'pathname'
 #
 # @author Andrew Johnson
 class GnomeWallpaperSlideshow
-  # Initializes a new wallpaper slideshow
-  # @param [String] filename The path to a wallpaper slideshow XML file.
-  #  Either pass the path to a non-existent file or omit the argument to
-  #  create a new slideshow.
-  def initialize(filename = "slideshow.xml")
-    @path = Pathname.new filename
+  attr_reader :path
+  
+  # Create a new GnomeWallpaperSlideshow
+  def initialize(&block)
+    instance_eval(&block)
+  end
 
-    if @path.exist?
-      File.open(@path, "r") do |slideshow_file|
-        # We want strict parsing to ensure that we're given an actual XML file
-        @xml_doc = Nokogiri::XML(slideshow_file) do |config|
-          config.strict
-        end
+  # Creates a new wallpaper slideshow
+  # @param [String] filename The path to the new file to create
+  #   Defaults to slideshow.xml in the current working directory
+  #   If the file already exists it will be overwritten
+  def create_new_slideshow(filename = "slideshow.xml")
+    @path = Pathname.new filename
+  end
+
+  # Loads an existing wallpaper slideshow
+  # @param [String] filename The path to the slideshow file
+  def load_slideshow(filename)
+    @path = Pathname.new filename
+    load_slideshow_xml
+  end
+
+  private
+
+  # Loads a wallpaper slideshow XML file
+  def load_slideshow_xml
+    File.open(self.path, "r") do |slideshow_file|
+      # We want strict parsing to ensure we are given a valid XML file
+      @xml_doc = Nokogiri::XML(slideshow_file) do |config|
+        config.strict
       end
     end
   end
